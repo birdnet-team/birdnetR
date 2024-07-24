89f03185a4c31eaabc0d63bc55f6a3d385cb9d3c
Stefan Kahl <kahst@hrz.tu-chemnitz.de>
2024-07-24 16:50:32 +0200

    fix float in interval string

--- R/birdnet_interface.R
+++ R/birdnet_interface.R
@@ -23,7 +23,7 @@ init_model <- function() {
 #' @param audio_path The path to the audio file.
 #' @return A list of predictions.
 #' @export
-predict_species <- function(model, audio_path = system.file("extdata", "soundscape.wav", package = "yourpackage")) {
+predict_species <- function(model, audio_path = system.file("extdata", "soundscape.wav", package = "birdnet")) {
   path <- py_pathlib$Path(audio_path)
   predictions <- model$predict_species_within_audio_file(path)
   return(predictions)
@@ -39,8 +39,15 @@ predict_species <- function(model, audio_path = system.file("extdata", "soundsca
 #' @return A list containing the top prediction and its confidence.
 #' @export
 get_top_prediction <- function(predictions, start_time, end_time) {
-  interval <- c(start_time, end_time)
-  prediction <- predictions[[interval]]
+  interval <- reticulate::tuple(start_time, end_time)
+  interval_str <- sprintf("(%.1f, %.1f)", start_time, end_time)
+
+  # Ensure the interval exists in the names of predictions
+  if (!interval_str %in% names(predictions)) {
+    stop("Interval not found in predictions")
+  }
+
+  prediction <- predictions[[interval_str]]
   top_prediction <- list(prediction = names(prediction)[1], confidence = unname(prediction)[1])
   return(top_prediction)
 }
