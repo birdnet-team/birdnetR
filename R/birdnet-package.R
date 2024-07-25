c0e09adee779939efac7e504a65a4051dc9fb2db
fegue <felix.gnthr@gmail.com>
2024-07-25 15:24:14 +0200

    use package file doc

---
+++ R/birdnet-package.R
@@ -0,0 +1,18 @@
+#' BirdNET for R
+#'
+#' `birdnetR` is a wrapper around the python package `birdnet`.
+#'
+#' @import reticulate
+#'
+#' @docType package
+#' @keywords internal
+#' @name birdnet
+#' @aliases birdnetR
+"_PACKAGE"
+
+## usethis namespace: start
+#' @importFrom reticulate configure_environment use_virtualenv import
+#' @importFrom reticulate use_python_version
+## usethis namespace: end
+NULL
+
