dfec69ded6ee39b9b16a2d3810bd95bebc1d4bd4
Sunny Tseng <sunnyyctseng@gmail.com>
2024-08-14 14:52:54 -0700

    fixed failed tests - load required python version and rbirdnet module

---
+++ tests/testthat/helper.R
@@ -0,0 +1,12 @@
+
+# Check that the required versions of Python and
+# the Python 'birdnet' module are installed; if not,
+# either print a message for the user instructing
+# them to install those, or see if we can do it
+# automatically.
+.check_birdnet_version()
+
+# Make sure reticulate uses the 'r-birdnet'
+# virtual environment when running tests.
+library(reticulate)
+reticulate::use_virtualenv("r-birdnet", required = TRUE)
