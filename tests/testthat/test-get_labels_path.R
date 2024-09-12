01d63c7b2580fb3a79680f7d2546eebec9730c3b
Felix Günther <felix.gnthr@gmail.com>
2024-09-12 16:16:47 +0200

    adds and updates tests

--- tests/testthat/test-get_labels_path.R
+++ tests/testthat/test-get_labels_path.R
@@ -1,11 +1,26 @@
 library(testthat)
 
+tflite_model <- birdnet_model_tflite(version = "v2.4")
+protobuf_model <- birdnet_model_protobuf(version = "v2.4")
+
+
 test_that("get_labels_path returns correct path for valid language", {
-  path <- get_labels_path("en_us")
+  path <- get_labels_path(model = tflite_model, language = "en_us")
+  expect_true(basename(path) == "en_us.txt")
+  expect_true(file.exists(path))
+
+  path <- get_labels_path(model = protobuf_model, language = "en_us")
+  expect_true(basename(path) == "en_us.txt")
   expect_true(file.exists(path))
 })
 
-test_that("get_labels_path throws an error for invalid language", {
+test_that("get_labels_path returns correct path for invalid language", {
+  expect_error(get_labels_path(model = tflite_model, language = "blonk"))
+  expect_error(get_labels_path(model = protobuf_model, language = "blonk"))
+})
+
+
+test_that("get_labels_path throws an error for character input", {
   expect_error(get_labels_path("invalid_language"))
 })
 
@@ -13,3 +28,5 @@ test_that("get_labels_path handles edge cases like empty string or NULL", {
   expect_error(get_labels_path(""))
   expect_error(get_labels_path(NULL))
 })
+
+# Missing test for get_labels_path with custom model
--- tests/testthat/test-module_map.R
+++ tests/testthat/test-module_map.R
@@ -1,24 +1,38 @@
-library(birdnetR)
 library(testthat)
 
 test_that("create_module_map", {
   module_map <- create_module_map("v2.4", "py_birdnet_models")
   expect_type(module_map, "list")
   expect_named(module_map, c("models", "misc"))
-  expect_named(module_map$models, c("tflite_v2.4", "protobuf_v2.4", "custom_v2.4", "raven_v2.4", "meta_v2.4"))
-  expect_named(module_map$misc, "available_languages_v2.4")
+  expect_named(
+    module_map$models,
+    c("tflite", "protobuf", "custom", "raven", "meta")
+  )
+})
+
+test_that("getting eelements from modules mpa", {
+  module_map <- create_module_map("v2.4", "py_birdnet_models")
+  tflite_model_path <- get_element_from_module_map(module_map, "models", "tflite")
+  expect_equal(
+    tflite_model_path,
+    "py_birdnet_models$v2m4$AudioModelV2M4TFLite"
+  )
 })
 
 
-test_that("get_model", {
-  module_map <- create_module_map("v2.4", "py_birdnet_models")
-  tflite_model_path <- get_model_from_module_map(module_map, "tflite_v2.4")
-  expect_type(tflite_model_path, "character")
-})
 
-
-test_that("get_misc", {
+test_that("all mapped modules can be evaluates", {
   module_map <- create_module_map("v2.4", "py_birdnet_models")
-  available_languages_path <- get_misc_from_module_map(module_map, "available_languages_v2.4")
-  expect_type(available_languages_path, "character")
+
+  # Evaluate all modules
+  for (module in unlist(module_map)) {
+    expect_s3_class(
+      evaluate_python_path(module),
+      c(
+        "python.builtin.type",
+        "python.builtin.object",
+        "python.builtin.set"
+      )
+    )
+  }
 })
