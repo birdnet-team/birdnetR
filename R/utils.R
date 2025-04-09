28c551f31cb35c65ec898f43a9b2ed939a960d15
Felix Günther <felix.gnthr@gmail.com>
2025-04-09 10:19:30 +0200

    remove \dontrun from example sin utils.R

--- R/utils.R
+++ R/utils.R
@@ -116,12 +116,11 @@ predictions_to_df <- function(predictions, keep_empty = FALSE) {
 #' @keywords internal
 #' @note This function is intended for internal use and may not be exported or accessible for external users.
 #' @examples
-#' \dontrun{
 #' birdnetR:::is_valid_species_list(c("species1", "species2")) # TRUE
 #' birdnetR:::is_valid_species_list(list("species1", "species2")) # TRUE
 #' birdnetR:::is_valid_species_list(c(1, 2, 3)) # FALSE
 #' birdnetR:::is_valid_species_list(list(a = 1, b = 2)) # FALSE
-#' }
+#'
 is_valid_species_list <- function(obj) {
   # Check if the object is a character vector of length > 0 and not a list
   is_vector <- is.vector(obj) &&
